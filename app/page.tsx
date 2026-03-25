db3d2e5f22b45e8d96e1698a4cad9b1c82cc6d2b
Jimmy Grados <jgrados@osiptel.gob.pe>
2026-03-25 18:33:02 -0500

    chore: remove deployment test marker

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import { ProtoAIWorkspace } from "@/components/prototype/protoai-workspace";
 
 export default function HomePage() {
-  return (
-    <>
-      <ProtoAIWorkspace />
-      <div className="visually-hidden" aria-hidden="true">build-check-github-ci-20260325-01</div>
-    </>
-  );
+  return <ProtoAIWorkspace />;
 }
